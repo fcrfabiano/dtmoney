15ecd9e0889b4a9ce3204074cb2ff5b1ad49f576
Fabiano César <fcfabiano.cesar@icloud.com>
2021-03-26 16:08:13 -0300

    Finalizado

---
+++ src/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createServer, Model } from 'miragejs';
+import { App } from './App';
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Salário',
+          type: 'deposit',
+          category: 'Receita',
+          amount: 2300,
+          createdAt: new Date('2021-03-05 10:30:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1500,
+          createdAt: new Date('2021-03-05 12:30:00'),
+        },
+        {
+          id: 3,
+          title: 'Aluguel Lindsey',
+          type: 'deposit',
+          category: 'Casa',
+          amount: 750,
+          createdAt: new Date('2021-03-06 16:00:00'),
+        }
+      ]
+    })
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
+    });
+  }
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
